053b39e5dbc2e25e9574a1272afa670ea617ce7d
RecursiveCollapse <samdockrill@gmail.com>
2025-01-30 10:16:13 -0500

    Initial Commit, created proj files and set gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NutriWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
