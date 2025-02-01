4ac37b2e83b22c9d793747162cbc02bf6c191e02
RecursiveCollapse <samdockrill@gmail.com>
2025-02-01 05:48:26 -0500

    Revert "Uncommented line"
    This reverts commit ba7503b668f090b76c68c30a39dd0eb1796661da.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace NutriWeek
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to exit...");
+            //Console.WriteLine("Press any key to exit...");
             //Console.ReadKey();
         }
     }
